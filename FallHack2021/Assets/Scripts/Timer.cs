d8c85294a456bbd6af876221a857cbe53f9c6582
tal8 <tal8@sfu.ca>
2021-11-27 12:06:23 -0800

    error fix

--- FallHack2021/Assets/Scripts/Timer.cs
+++ FallHack2021/Assets/Scripts/Timer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] float LevelTimer = 30f;
+    [SerializeField] float levelTimer = 30f;
     public float fillFraction;
     float timerValue;
 
@@ -24,7 +24,7 @@ public class Timer : MonoBehaviour
 
         if(isAlive){
             if(timerValue > 0){
-                fillFraction = timerValue/timeToComplete;
+                fillFraction = timerValue/levelTimer;
             }else{
                 isAlive = false;
                 timerValue = LevelTimer;
